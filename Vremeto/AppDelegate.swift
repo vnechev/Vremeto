407fd86cf942d4103c65317d60407ad56d06bfeb
Vasil Nechev <vnechev@gmail.com>
2017-10-03 12:17:26 +0300

    Initial Commit

---
+++ Vremeto/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Vremeto
+//
+//  Created by Vasil Nechev on 3.10.17 г..
+//  Copyright © 2017 г. Vasil Nechev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
